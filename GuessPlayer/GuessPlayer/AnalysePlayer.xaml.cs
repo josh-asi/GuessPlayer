b95844a3f71c0ee51cd75a296230b82054d75e05
Josh Asi <Josh Asi>
2017-07-29 17:03:49 +1200

    Add project files.

---
+++ GuessPlayer/GuessPlayer/AnalysePlayer.xaml.cs
@@ -0,0 +1,103 @@
+﻿using GuessPlayer.Model;
+using Newtonsoft.Json;
+using Plugin.Media;
+using Plugin.Media.Abstractions;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace GuessPlayer
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class AnalysePlayer : ContentPage
+    {
+        public AnalysePlayer()
+        {
+            InitializeComponent();
+        }
+
+
+        private async void OpenCamera(object sender, EventArgs e)
+        {
+            await CrossMedia.Current.Initialize();
+
+            if (!CrossMedia.Current.IsCameraAvailable || !CrossMedia.Current.IsTakePhotoSupported)
+            {
+                await DisplayAlert("No Camera", ":( No camera available.", "OK");
+                return;
+            }
+
+            MediaFile file = await CrossMedia.Current.TakePhotoAsync(new StoreCameraMediaOptions
+            {
+                PhotoSize = PhotoSize.Medium,
+                Directory = "Sample",
+                Name = $"{DateTime.UtcNow}.jpg"
+            });
+
+            if (file == null)
+                return;
+
+            image.Source = ImageSource.FromStream(() =>
+            {
+                return file.GetStream();
+            });
+
+
+            await MakePredictionRequest(file);
+        }
+
+        static byte[] GetImageAsByteArray(MediaFile file)
+        {
+            var stream = file.GetStream();
+            BinaryReader binaryReader = new BinaryReader(stream);
+            return binaryReader.ReadBytes((int)stream.Length);
+        }
+
+        async Task MakePredictionRequest(MediaFile file)
+        {
+            var client = new HttpClient();
+
+            client.DefaultRequestHeaders.Add("Prediction-Key", "3ddfa47884684292b4fd325263521824");
+
+            string url = "https://southcentralus.api.cognitive.microsoft.com/customvision/v1.0/Prediction/718f410b-b0dd-48aa-a569-521a0d5a2a4f/image?iterationId=fc1a0140-6fd4-4d99-b16c-a493fc4f3f45";
+
+            HttpResponseMessage response;
+
+            byte[] byteData = GetImageAsByteArray(file);
+
+            using (var content = new ByteArrayContent(byteData))
+            {
+
+                content.Headers.ContentType = new MediaTypeHeaderValue("application/octet-stream");
+                response = await client.PostAsync(url, content);
+
+
+                if (response.IsSuccessStatusCode)
+                {
+                    var responseString = await response.Content.ReadAsStringAsync();
+
+                    EvaluationModel responseModel = JsonConvert.DeserializeObject<EvaluationModel>(responseString);
+
+                    //var prob = from p in responseModel.Predictions select p.Probability;
+                    var Tag = from t in responseModel.Predictions select t.Tag;
+
+
+                    Player.Text = Tag.ElementAt(0);
+
+                    
+                }
+
+                //Get rid of file once we have finished using it
+                file.Dispose();
+            }
+        }
+    }
+}
---
+++ GuessPlayer/GuessPlayer/Model/EvaluationModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GuessPlayer.Model
+{
+    public class EvaluationModel
+    {
+        public string Id { get; set; }
+        public string Project { get; set; }
+        public string Iteration { get; set; }
+        public string Created { get; set; }
+        public List<Prediction> Predictions { get; set; }
+    }
+
+    public class Prediction
+    {
+        public string TagId { get; set; }
+        public string Tag { get; set; }
+        public double Probability { get; set; }
+    }
+}
